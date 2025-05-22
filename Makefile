8d001e4aa8f21ae3eb5c84d621f191a251190f98
jamesread <contact@jread.com>
2025-05-22 23:58:10 +0100

    Fix build with missing make targets

--- Makefile
+++ Makefile
@@ -3,3 +3,16 @@ buildah:
 
 docker:
 	docker build . -t jamesread/prometheus-github-exporter-micro
+
+
+tests-debian:
+	#python3-coverage run --branch -m pytest
+
+tests:
+	coverage run --branch -m pytest
+
+lint:
+	pylint-3 .
+
+lint-debian:
+	#pylint .
--- prometheus-github-exporter-micro.py
+++ prometheus-github-exporter-micro.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import requests
 import logging
 import os
 import json
 from datetime import datetime
 from time import sleep
+
 from dateutil import parser
 
 from prometheus_client import start_http_server, Gauge
+
+import requests
 
 def get_enabled_workflows(repo):
     base_url = "https://api.github.com/repos/" + repo
@@ -18,6 +20,7 @@ def get_enabled_workflows(repo):
     workflows = response.json().get("workflows", [])
     return [wf for wf in workflows if wf.get("state") == "active"]
 
+
 def get_latest_run(workflow_id):
     BASE_URL = "https://api.github.com/repos/" + repo
 
@@ -26,6 +29,7 @@ def get_latest_run(workflow_id):
     response.raise_for_status()
     runs = response.json().get("workflow_runs", [])
     return runs[0] if runs else None
+
 
 def get_gauge(repo, metric):
     global GAUGE_REGISTRY
@@ -39,7 +43,8 @@ def get_gauge(repo, metric):
 
     return GAUGE_REGISTRY[metric].labels(repo=repo)
 
-def get_workflow_gauge(repo, workflow, metric ):
+
+def get_workflow_gauge(repo, workflow, metric):
     global GAUGE_REGISTRY
 
     if metric not in GAUGE_REGISTRY:
@@ -52,20 +57,23 @@ def get_workflow_gauge(repo, workflow, metric ):
 
     return GAUGE_REGISTRY[metric].labels(repo=repo, workflow=workflow)
 
-GAUGE_REGISTRY = dict()
+
+GAUGE_REGISTRY = {}
 REPOS = os.getenv("REPOS", "").split(",")
 USERS = os.getenv("USERS", "").split(",")
 PROM_PORT = int(os.getenv("PROM_PORT", "9171"))
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
 
-if GITHUB_TOKEN == "": 
+if GITHUB_TOKEN == "":
     REQUEST_HEADERS = {}
-else: 
+else:
     REQUEST_HEADERS = {"Authorization": "token " + GITHUB_TOKEN}
 
 logging.getLogger().setLevel(20)
-logging.info("Starting prometheus github exporter (micro) on port %s", PROM_PORT)
+logging.info(
+    "Starting prometheus github exporter (micro) on port %s", PROM_PORT)
 start_http_server(PROM_PORT)
+
 
 def get_repos(repos, users):
     ret = set()
@@ -80,7 +88,8 @@ def get_repos(repos, users):
 
             sleep(3)
 
-            res = requests.get(f"https://api.github.com/users/{user}/repos?page={page}", headers = REQUEST_HEADERS)
+            res = requests.get(
+                f"https://api.github.com/users/{user}/repos?page={page}", headers=REQUEST_HEADERS)
 
             if res.status_code != 200:
                 logging.warning("API HTTP Status: %s", res.status_code)
@@ -91,7 +100,7 @@ def get_repos(repos, users):
             except json.decoder.JSONDecodeError as e:
                 logging.warning("Failed to decode GitHub JSON: %s", str(e))
                 continue
-            
+
             if not github_repos:
                 break
 
@@ -107,7 +116,8 @@ while True:
     for repo in get_repos(REPOS, USERS):
         logging.info("Updating %s", repo)
 
-        res = requests.get("https://api.github.com/repos/" + repo, headers = REQUEST_HEADERS)
+        res = requests.get("https://api.github.com/repos/" +
+                           repo, headers=REQUEST_HEADERS)
 
         if res.status_code != 200:
             logging.warning("API HTTP Status: %s", res.status_code)
@@ -123,13 +133,14 @@ while True:
         get_gauge(repo, "issues").set(github_repo['open_issues_count'])
         get_gauge(repo, "forks").set(github_repo['forks'])
         get_gauge(repo, "subscribers").set(github_repo['subscribers_count'])
-        
+
         for wf in get_enabled_workflows(repo):
             logging.info("Updating workflow %s : %s", repo, wf['name'])
 
             latest_run = get_latest_run(wf['id'])
             if latest_run:
-                logging.info("Latest run for workflow %s: %s", wf['name'], latest_run['status'])
+                logging.info("Latest run for workflow %s: %s",
+                             wf['name'], latest_run['status'])
 
                 res = 1 if latest_run['conclusion'] == 'success' else 0
 
@@ -140,13 +151,15 @@ while True:
             else:
                 logging.warning("No runs found for workflow %s", wf['name'])
 
-    
-    res = requests.get("https://api.github.com/rate_limit", headers = REQUEST_HEADERS)
+    res = requests.get("https://api.github.com/rate_limit",
+                       headers=REQUEST_HEADERS)
     rate_limit = json.loads(res.text)['resources']['core']
-    logging.info("Rate limit status: %s out of %s", rate_limit['used'], rate_limit['limit'])
+    logging.info("Rate limit status: %s out of %s",
+                 rate_limit['used'], rate_limit['limit'])
 
-    if rate_limit['used'] >= rate_limit['limit']: 
-        logging.warning("Rate limit will reset at: %s", datetime.fromtimestamp(rate_limit['reset']))
+    if rate_limit['used'] >= rate_limit['limit']:
+        logging.warning("Rate limit will reset at: %s",
+                        datetime.fromtimestamp(rate_limit['reset']))
 
     sleepySeconds = os.getenv("UPDATE_DELAY_SECONDS", 3600)
     logging.info("Finished updates. Sleeping for %s seconds", sleepySeconds)
